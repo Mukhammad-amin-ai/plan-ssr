749b52e7481906cbf8bcdca7e667125d3d2c917a
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-31 11:54:25 +0500

    main

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  alias:{
+    "@": resolve(__dirname, "/"),
+  },
+  css:["~/assets/style/main.css"],
+})
